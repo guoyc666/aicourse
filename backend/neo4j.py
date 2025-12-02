25a41a7ded36dcf5b02d4297b74358c3e3e59aee
张诏 <3370737400@@qq.com>
2025-12-02 14:03:12 +0800

    解决 neo4j 用户名密码配置错误

--- backend/neo4j.py
+++ backend/neo4j.py
@@ -2,7 +2,7 @@ from py2neo import Graph
 import os
 
 NEO4J_URI = os.getenv("NEO4J_URI", "bolt://localhost:7687")
-NEO4J_USER = os.getenv("NEO4J_USER", "neo4j")
+NEO4J_USER = os.getenv("NEO4J_USER", "aicourse")
 NEO4J_PASSWORD = os.getenv("NEO4J_PASSWORD", "aicourse")
 
 graph = Graph(NEO4J_URI, auth=(NEO4J_USER, NEO4J_PASSWORD))
